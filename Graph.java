d662eb89cf82d399a0f74658121c79d41d2eecef
rohitrfactor <rohitrfactor@gmail.com>
2016-11-24 21:32:44 +0530

    Add Graph

---
+++ Graph.java
@@ -0,0 +1,13 @@
+public class Graph {
+
+    private final List<Edge> edges;
+
+    public Graph(List<Edge> edges) {
+        this.edges = edges;
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+
+}
